3dbb24aa11643cc8051f8c7c1dbb19ed9eb1a33a
denis.bosyi <denis.bosiy@gmail.com>
2023-10-08 18:54:49 +0300

    Добавлена основа для фронтенд части

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <p>
+         Змінити <code>src/App.tsx</code> та зберегти для перезавантаження.
+        </p>
+        <a className="app-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Вчити React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
