4bc4c926837e68c00c15a75380251e92f0cf5841
denis.bosyi <denis.bosiy@gmail.com>
2023-10-19 00:38:09 +0300

    init commit

--- Frontend/src/icons/index.ts
+++ Frontend/src/icons/index.ts
@@ -1,3 +1,5 @@
+export {ReactComponent as ArrowDownIcon} from "./arrow-down.svg";
+export {ReactComponent as ArrowUpIcon} from "./arrow-up.svg";
 export {ReactComponent as HiddenEyeIcon} from "./hidden-eye.svg";
 export {ReactComponent as MagnifierIcon} from "./magnifier.svg";
 export {ReactComponent as OpenedEyeIcon} from "./opened-eye.svg";
