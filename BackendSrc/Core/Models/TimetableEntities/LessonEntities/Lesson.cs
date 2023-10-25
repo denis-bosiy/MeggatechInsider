66837be0ac8d39ed0d091f3e69ee3eeea817ac07
Павел Ермаков <pavel.ermakov@travelline.ru>
2023-10-25 13:23:12 +0300

    [MEG-9] Убрал копию LessonType'а в Core

--- BackendSrc/Core/Models/TimetableEntities/LessonEntities/Lesson.cs
+++ BackendSrc/Core/Models/TimetableEntities/LessonEntities/Lesson.cs
@@ -1,20 +1,20 @@
 using Core.Models.SubjectEntities;
 using Core.Models.TeacherEntities;
 using Core.Models.TimetableEntities.StudentGroupEntities;
+using Domain.TimetableEntities.LessonEntities;
 
-namespace Core.Models.TimetableEntities.LessonEntities
+namespace Core.Models.TimetableEntities.LessonEntities;
+
+public class Lesson : Entity
 {
-    public class Lesson : Entity
-    {
-        public LessonType LessonType { get; set; }
-        public TimeOnly StartTime { get; set; }
-        public TimeOnly EndTime { get; set; }
-        public int StudentGroupId { get; set; }
-        public StudentGroup StudentGroup { get; set; }
-        public int SubjectId { get; set; }
-        public Subject Subject { get; set; }
-        public int TeacherId { get; set; }
-        public Teacher Teacher { get; set; }
-        public int Classroom { get; set; }
-    }
+    public LessonType LessonType { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+    public int StudentGroupId { get; set; }
+    public StudentGroup StudentGroup { get; set; }
+    public int SubjectId { get; set; }
+    public Subject Subject { get; set; }
+    public int TeacherId { get; set; }
+    public Teacher Teacher { get; set; }
+    public int Classroom { get; set; }
 }
