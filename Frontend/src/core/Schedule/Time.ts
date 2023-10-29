0a452038520542d9795f99061582edd04a33fb59
denis.bosyi <denis.bosiy@gmail.com>
2023-10-30 02:28:10 +0300

    init commit

---
+++ Frontend/src/core/Schedule/Time.ts
@@ -0,0 +1,9 @@
+export class Time {
+  public hours: number;
+  public minutes: number;
+
+  constructor(_hours: number, _minutes: number) {
+    this.hours = _hours;
+    this.minutes = _minutes;
+  }
+}
---
+++ Frontend/src/core/Schedule/Workday.ts
@@ -0,0 +1,7 @@
+export enum Workday {
+    Monday = "Понедельник",
+    Tuesday = "Вторник",
+    Wednesday = "Среда",
+    Thursday = "Четверг",
+    Friday = "Пятница",
+};
