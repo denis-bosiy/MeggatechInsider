bd948572c48664c48fbfe3b9afcaa8fdfd3f4b00
denis.bosyi <denis.bosiy@gmail.com>
2023-10-30 20:47:16 +0300

    Исправлены ошибки eslint-a

--- Frontend/src/pages/TestPage/model/reducer.ts
+++ Frontend/src/pages/TestPage/model/reducer.ts
@@ -1,35 +1,32 @@
-import {Action, TEST_PAGE_ACTIONS} from "./actions";
-import {TestPageData} from "./types";
+import { Action, TEST_PAGE_ACTIONS } from "./actions";
+import { TestPageData } from "./types";
 
 const initData: TestPageData = {
   login: "",
-  password: "",
+  password: ""
 };
 
 const testPageReducer = (state = initData, action: Action) => {
   switch (action.type) {
-  case TEST_PAGE_ACTIONS.TEST_PAGE_SET_LOGIN:
-    return {
-      ...state,
-      login: action.payload,
-
-    };
-  case TEST_PAGE_ACTIONS.TEST_PAGE_SET_PASSWORD:
-    return {
-      ...state,
-      password: action.payload,
-    };
-  case TEST_PAGE_ACTIONS.TEST_PAGE_RESET:
-    return {
-      ...state,
-      login: "",
-      password: "",
-    };
-  default:
-    return state;
+    case TEST_PAGE_ACTIONS.TEST_PAGE_SET_LOGIN:
+      return {
+        ...state,
+        login: action.payload
+      };
+    case TEST_PAGE_ACTIONS.TEST_PAGE_SET_PASSWORD:
+      return {
+        ...state,
+        password: action.payload
+      };
+    case TEST_PAGE_ACTIONS.TEST_PAGE_RESET:
+      return {
+        ...state,
+        login: "",
+        password: ""
+      };
+    default:
+      return state;
   }
 };
 
-export {
-  testPageReducer,
-};
+export { testPageReducer };
