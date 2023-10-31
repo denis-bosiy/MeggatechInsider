1d059a0123bfac87355d5d17badf6ffc8a12e8a2
Adel Sibgatullin <adelsibga@gmail.com>
2023-10-31 17:55:11 +0300

    login-page Добавил страницу логинации Добавил className для Notification и для Input

--- Frontend/src/pages/SignInPage/SignInPage.tsx
+++ Frontend/src/pages/SignInPage/SignInPage.tsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import "./SignInPage.scss";
+import Button, { ButtonSize, ButtonType } from "../../components/Button/Button";
+import Input, { InputType } from "../../components/Input/Input";
+import Notification from "../../components/Notification/Notification";
 
 const SignInPage = () => {
+  const [passwordInputValue, setPasswordInputValue] = useState<string>("");
+
   return (
     <>
-      <input type="password" />
+      <div className='wrapper'>
+        <Notification
+          className='sign-in__notification'
+          title='Ошибка'
+          description='Введен неправильный пароль'
+        />
+        <div className="sign-in">
+          <h1 className="sign-in__title">
+            Пароль
+          </h1>
+          <Input
+            className='sign-in__input'
+            value={passwordInputValue}
+            type={InputType.Password}
+            placeholder="Введите пароль"
+            onValueChange={setPasswordInputValue}
+          />
+          <Button
+            className='sign-in__button'
+            type={ButtonType.Primary}
+            size={ButtonSize.Default}
+            label='Войти'
+          />
+        </div>
+      </div>
     </>
   );
 };
