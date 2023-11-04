60dca26731e756f3dffed1945198ad35e2950b6a
AbushaevaDiana <79981087+AbushaevaDiana@users.noreply.github.com>
2023-11-04 22:57:55 +0300

    Feature/43475474 teacher guidebook timetable (#37)
    * Добавила страницу Справочник по преподавателям в расписании. Таблицу в ней
    
    * Доработала цвета, добавила id, изменила время на ткаой же формат как в настройках расписания. Вынесла в переменные.
    
    * Поправила форматирование

--- Frontend/src/redux/reducers.ts
+++ Frontend/src/redux/reducers.ts
@@ -1,8 +1,10 @@
 import {combineReducers} from "redux";
 import {testPageReducer} from "../pages/TestPage/model/reducer";
 import {subjectsSyllabusPageReducer} from "../pages/SubjectsSyllabusPage/model/reducer";
+import {teacherGuidebookTimetablePageReducer} from "../pages/TeacherGuidebookTimetablePage/model/reducer";
 
 export default combineReducers({
   testPageStore: testPageReducer,
   subjectsSyllabusPageStore: subjectsSyllabusPageReducer,
+  teacherGuidebookTimetablePageStore: teacherGuidebookTimetablePageReducer,
 });
