e8bf16ba7e85cdc03859e54052d154eb24425ed0
AlekseiPavlovskii <128834478+AlekseiPavlovskii@users.noreply.github.com>
2023-11-13 13:36:23 +0300

    MEG-18 Добавлена сущность назначения

---
+++ BackendSrc/Domain/AssignmentEntities/Assignment.cs
@@ -0,0 +1,16 @@
+using Domain.SubjectEntities;
+using Domain.TeacherEntities;
+
+namespace Domain.AssignmentEntities
+{
+    public class Assignment : Entity
+    {
+        public Teacher Teacher { get; set; }
+        public int TeacherId => Teacher.Id;
+
+        public Subject Subject { get; set; }
+        public int SubjectId => Subject.Id;
+
+        public int GroupCount { get; set; }
+    }
+}
