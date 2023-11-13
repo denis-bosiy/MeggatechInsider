ac360c49275e9e59d55cfdc3fd8be3ca458d6f07
Maxim <masya.prozorov@mail.ru>
2023-11-13 05:56:55 +0300

    Внесены правки после ревью, обновил ветку до актуального состояния

--- BackendSrc/Api/Controllers/TimetableController.cs
+++ BackendSrc/Api/Controllers/TimetableController.cs
@@ -396,4 +396,98 @@ public class TimetableController : ControllerBase
 
         return Ok( responseDto );
     }
+
+    [HttpGet( "teachers" )]
+    [ResponseType( typeof( TeacherTimetableListResponseDto ) )]
+    public IActionResult GetTeachers( [FromQuery] TeacherTimetableListRequestDto teacherTimetableRequest )
+    {
+        // mock
+        // Вызываем сервис для получения преподавателей
+
+        return Ok( new TeacherTimetableListResponseDto()
+        {
+            Teachers = new List<TeacherTimetableDto>
+            {
+                new TeacherTimetableDto()
+                {
+                    Id = 1,
+                    SubjectName = "Физика",
+                    SubjectId = 1,
+                    TeacherName = "Prozorov Maxim Andreevich",
+                    TeacherId = 1,
+                    AvailableHours = new List<AvailableHoursDto>()
+                    {
+                        new AvailableHoursDto()
+                        {
+                            Id = 1,
+                            WeekDay = DayOfWeek.Sunday,
+                            StartTime = new TimeOnly(09, 00),
+                            EndTime = new TimeOnly(09, 45)
+                        },
+                        new AvailableHoursDto()
+                        {
+                            Id = 2,
+                            WeekDay = DayOfWeek.Sunday,
+                            StartTime = new TimeOnly(10, 00),
+                            EndTime = new TimeOnly(10, 45)
+                        },
+                        new AvailableHoursDto()
+                        {
+                            Id = 3,
+                            WeekDay = DayOfWeek.Sunday,
+                            StartTime = new TimeOnly(11, 00),
+                            EndTime = new TimeOnly(11, 45)
+                        }
+                    },
+                    DistributedHoursToPlan = 10,
+                    HoursToPlan = 11,
+                    CreditHours = 12,
+                    WorkedOverPlan = 13
+                }
+            }
+        } );
+    }
+
+    [HttpPatch( "set-hours" )]
+    public IActionResult SetAvailableHours( TeacherTimetableSaveRequestDto teacherTimetableRequest )
+    {
+        // mock
+        // Вызываем сервис для сохранения изменений
+        return Ok();
+    }
+
+    [HttpGet( "available-hours" )]
+    [ResponseType( typeof( AvailableHoursResponseListDto ) )]
+    public IActionResult GetAvailableHours ()
+    {
+        // mock
+        // Вызываем сервис для получения времени
+        return Ok( new AvailableHoursResponseListDto()
+        {
+            AvailableHours = new List<AvailableHoursDto>()
+                    {
+                        new AvailableHoursDto()
+                        {
+                            Id = 1,
+                            WeekDay = DayOfWeek.Sunday,
+                            StartTime = new TimeOnly(09, 00),
+                            EndTime = new TimeOnly(09, 45)
+                        },
+                        new AvailableHoursDto()
+                        {
+                            Id = 2,
+                            WeekDay = DayOfWeek.Sunday,
+                            StartTime = new TimeOnly(10, 00),
+                            EndTime = new TimeOnly(10, 45)
+                        },
+                        new AvailableHoursDto()
+                        {
+                            Id = 3,
+                            WeekDay = DayOfWeek.Sunday,
+                            StartTime = new TimeOnly(11, 00),
+                            EndTime = new TimeOnly(11, 45)
+                        }
+                    },
+        } ); 
+    }
 }
