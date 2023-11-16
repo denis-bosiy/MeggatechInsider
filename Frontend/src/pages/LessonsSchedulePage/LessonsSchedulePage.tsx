265afe281ab3871bf09ce6aeddb6f279f6f4b365
denis.bosyi <denis.bosiy@gmail.com>
2023-11-17 01:53:35 +0300

    Добавлена страница расписания

--- Frontend/src/pages/LessonsSchedulePage/LessonsSchedulePage.tsx
+++ Frontend/src/pages/LessonsSchedulePage/LessonsSchedulePage.tsx
@@ -1,10 +1,24 @@
-import React from "react";
-import { ScheduleContainerComponent } from "../../components/Schedule/ScheduleContainer";
+import React, { useContext } from "react";
+import { ScheduleComponent } from "../../components/Schedule/Schedule";
+import { useDispatch, useSelector } from "react-redux";
+import { ISchedule } from "../../core/Schedule/ISchedule";
+import { ActionBuilder } from "./model/actions";
+import ModalSettingsContext from "../../utils/ModalSettingsContext";
+import AgreementModalView from "../../components/AgreementModalView/AgreementModalView";
 
 const LessonsSchedulePage = (): JSX.Element => {
+  const schedule: ISchedule = useSelector((state: { lessonsScheduleStore: ISchedule }) => state.lessonsScheduleStore);
+  const dispatch = useDispatch();
+  const { openModal } = useContext(ModalSettingsContext);
+
+  const handleDeleteLesson = (lessonId: string): void => {
+    openModal("Удалить", <AgreementModalView proceedAction={() => dispatch(ActionBuilder.deleteLesson(lessonId))} />);
+  };
+
+  console.log(schedule);
   return (
     <>
-      <ScheduleContainerComponent />
+      <ScheduleComponent schedule={schedule} handleDeleteLesson={handleDeleteLesson} />
     </>
   );
 };
