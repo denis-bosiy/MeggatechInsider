0eeb31b8be841b50cae5cd047816d3ebdffc2bd6
Yaroslav <Yaroslav.Kuzmin.01@yandex.ru>
2023-11-16 17:35:15 +0300

    44847565 Переписал модельку

--- Frontend/src/pages/CoursesTimetable/TeacherGuidebookCoursesTimetablePage/TeacherGuidebookCoursesTimetablePage.tsx
+++ Frontend/src/pages/CoursesTimetable/TeacherGuidebookCoursesTimetablePage/TeacherGuidebookCoursesTimetablePage.tsx
@@ -1,5 +1,5 @@
 import {useSelector} from "react-redux";
-import {TeacherGuidebookTimetablePageData} from "./model/types";
+import {TeacherGuidebookCoursesTimetablePageData} from "./model/types";
 import {classNames} from "../../../utils/classNames";
 import ActionButton, {ActionButtonType} from "../../../components/ActionButton/ActionButton";
 import {PenIcon} from "../../../icons/index";
@@ -8,13 +8,14 @@ import Input, {InputType} from "../../../components/Input/Input";
 
 const Guidebook = () => {
   const guidebook = useSelector((state: {
-    teacherGuidebookTimetablePageStore: TeacherGuidebookTimetablePageData
-  }) => state.teacherGuidebookTimetablePageStore);
+    teacherGuidebookCoursesTimetablePageStore: TeacherGuidebookCoursesTimetablePageData
+  }) => state.teacherGuidebookCoursesTimetablePageStore);
 
   return (
     guidebook.map((teacher) => (
       <tr className="row" key={teacher.id}>
-        <td className="cell">{teacher.subjectName}</td>
+        <td className="cell">{teacher.course}</td>
+        <td className="cell">{teacher.type}</td>
         <td className="cell">{teacher.teacherName}</td>
         <td className="cell">
           {teacher.availableHours.map((time) => (
@@ -26,7 +27,6 @@ const Guidebook = () => {
         </td>
         <td className="cell">{teacher.hoursToPlan}</td>
         <td className="cell">{teacher.creditHours}</td>
-        <td className="cell">{teacher.workedOverPlan}</td>
       </tr>
     ))
   );
@@ -88,13 +88,13 @@ const TeacherGuidebookCoursesTimetablePage = () => {
       <table className="table">
         <thead className="header">
           <tr className="row">
-            <th className="cell">Предмет</th>
+            <th className="cell">Курс</th>
+            <th className="cell">Тип</th>
             <th className="cell">Преподаватель</th>
-            <th className="cell">Доступные<br/>часы</th>
-            <th className="cell">Кол-во распределенных<br/>часов в неделю по плану</th>
-            <th className="cell">Кол-во часов<br/>в неделю по плану</th>
-            <th className="cell">Кол-во часов<br/>долга</th>
-            <th className="cell">Кол-во часов<br/>переработка</th>
+            <th className="cell">Доступные часы</th>
+            <th className="cell">Кол-во распределенных часов в неделю по плану</th>
+            <th className="cell">Кол-во часов в неделю по плану</th>
+            <th className="cell">Кол-во часов долга</th>
           </tr>
         </thead>
         <tbody>
