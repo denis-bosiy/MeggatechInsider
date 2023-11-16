67f7289297c8ccf692bbc80969fa547a07f55d03
Yaroslav <Yaroslav.Kuzmin.01@yandex.ru>
2023-11-16 18:57:37 +0300

    44847565 Добавил CTable

--- Frontend/src/pages/CoursesTimetable/TeacherGuidebookCoursesTimetablePage/TeacherGuidebookCoursesTimetablePage.tsx
+++ Frontend/src/pages/CoursesTimetable/TeacherGuidebookCoursesTimetablePage/TeacherGuidebookCoursesTimetablePage.tsx
@@ -5,31 +5,135 @@ import ActionButton, {ActionButtonType} from "../../../components/ActionButton/A
 import {PenIcon} from "../../../icons/index";
 import React, {useState} from "react";
 import Input, {InputType} from "../../../components/Input/Input";
+import {CTableBuilder} from "../../../core/Table/CTableBuilder";
+import {CTable} from "../../../core/Table/CTable";
+import {CTableManager} from "../../../core/Table/CTableManager";
+import {TableType} from "../../../core/Table/TableType";
+import {SortingOrder} from "../../../core/Table/SortingOrder";
 
-const Guidebook = () => {
-  const guidebook = useSelector((state: {
+interface TableProps {
+  isEdited: boolean;
+  setIsEdited: (isEdited: boolean) => void;
+}
+
+const Table = ({isEdited, setIsEdited}: TableProps) => {
+  const data = useSelector((state: {
     teacherGuidebookCoursesTimetablePageStore: TeacherGuidebookCoursesTimetablePageData
   }) => state.teacherGuidebookCoursesTimetablePageStore);
 
-  return (
-    guidebook.map((teacher) => (
-      <tr className="row" key={teacher.id}>
-        <td className="cell">{teacher.course}</td>
-        <td className="cell">{teacher.type}</td>
-        <td className="cell">{teacher.teacherName}</td>
-        <td className="cell">
-          {teacher.availableHours.map((time) => (
-            <p key={time.id}>{time.weekDay} {time.startTime} - {time.endTime}</p>))}
-        </td>
-        <td className={classNames("cell" + (teacher.distributedHoursToPlan < teacher.hoursToPlan ? " -error" : "")
-          + (teacher.distributedHoursToPlan > teacher.hoursToPlan ? " -warning" : ""))}>
-          {teacher.distributedHoursToPlan}
-        </td>
-        <td className="cell">{teacher.hoursToPlan}</td>
-        <td className="cell">{teacher.creditHours}</td>
-      </tr>
-    ))
+  const [tableData, setTableData] = useState(structuredClone(data));
+  const teacherGuidebookCoursesTimetableTableBuilder: CTableBuilder = new CTableBuilder(
+    tableData,
+    setTableData
   );
+  teacherGuidebookCoursesTimetableTableBuilder.addSearchFeature();
+  teacherGuidebookCoursesTimetableTableBuilder.addEditFeature({
+    value: isEdited,
+  }, ({value}) => setIsEdited(value));
+
+  const teacherGuidebookCoursesTimetableTable: CTable = teacherGuidebookCoursesTimetableTableBuilder.getTable();
+  const teacherGuidebookCoursesTimetableTableManager: CTableManager = new CTableManager(teacherGuidebookCoursesTimetableTable);
+
+  const handleSort = (columnName: string): void => {
+    console.log(123);
+    teacherGuidebookCoursesTimetableTableManager.invokeFunction(
+      "sort",
+      TableType.Default,
+      [columnName, SortingOrder.Ascending],
+    );
+  };
+
+  const rows = data.map((item) => (
+    <tr className="row" key={item.id}>
+      <td className="cell">
+        {item.course}
+      </td>
+      <td className="cell">{item.type}</td>
+      <td className="cell">{item.teacherName}</td>
+      <td className="cell">
+        {item.availableHours.map((time) => (
+          <p key={time.id}>{time.weekDay} {time.startTime} - {time.endTime}</p>))}
+      </td>
+      <td className={classNames("cell" + (item.distributedHoursToPlan < item.hoursToPlan ? " -error" : "")
+              + (item.distributedHoursToPlan > item.hoursToPlan ? " -warning" : ""))}>
+        {item.distributedHoursToPlan}
+      </td>
+      <td className="cell">{item.hoursToPlan}</td>
+      <td className="cell">{item.creditHours}</td>
+    </tr>
+  ));
+
+  return <>
+    <thead className="header">
+      <tr className="row">
+        <th
+          className="cell -filter"
+          onClick={() => handleSort("course")}
+        >
+          Курс
+        </th>
+        <th
+          className="cell -filter"
+          onClick={() => handleSort("type")}
+        >
+          Тип
+        </th>
+        <th
+          className="cell -filter"
+          onClick={() => handleSort("teacherName")}
+        >
+          Преподаватель
+        </th>
+        <th
+          className="cell -filter"
+          onClick={() => handleSort("availableHours")}
+        >
+          Доступные часы
+        </th>
+        <th
+          className="cell -filter"
+          onClick={() => handleSort("distributedHoursToPlan")}
+        >
+          Кол-во распределенных часов в неделю по плану
+        </th>
+        <th
+          className="cell -filter"
+          onClick={() => handleSort("hoursToPlan")}
+        >
+          Кол-во часов в неделю по плану
+        </th>
+        <th
+          className="cell -filter"
+          onClick={() => handleSort("creditHours")}
+        >
+          Кол-во часов долга
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      {rows}
+    </tbody>
+  </>;
+
+  // return (
+  //   guidebook.map((teacher) => (
+  //     <tr className="row" key={teacher.id}>
+  //       <td className="cell">{teacher.course}</td>
+  //       <td className="cell">{teacher.type}</td>
+  //       <td className="cell">{teacher.teacherName}</td>
+  //       <td className="cell">
+  //         {teacher.availableHours.map((time) => (
+  //           <p key={time.id}>{time.weekDay} {time.startTime} - {time.endTime}</p>))}
+  //       </td>
+  //       <td className={classNames("cell" + (teacher.distributedHoursToPlan < teacher.hoursToPlan ? " -error" : "")
+  //         + (teacher.distributedHoursToPlan > teacher.hoursToPlan ? " -warning" : ""))}>
+  //         {teacher.distributedHoursToPlan}
+  //       </td>
+  //       <td className="cell">{teacher.hoursToPlan}</td>
+  //       <td className="cell">{teacher.creditHours}</td>
+  //     </tr>
+  //   ))
+  // );
 };
 
 const TeacherGuidebookTimetableButton = (isEdited: boolean, changeIsEdited: () => void) => {
@@ -86,20 +190,10 @@ const TeacherGuidebookCoursesTimetablePage = () => {
         </div>
       </div>
       <table className="table">
-        <thead className="header">
-          <tr className="row">
-            <th className="cell">Курс</th>
-            <th className="cell">Тип</th>
-            <th className="cell">Преподаватель</th>
-            <th className="cell">Доступные часы</th>
-            <th className="cell">Кол-во распределенных часов в неделю по плану</th>
-            <th className="cell">Кол-во часов в неделю по плану</th>
-            <th className="cell">Кол-во часов долга</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Guidebook()}
-        </tbody>
+        <Table
+          isEdited={isEdited}
+          setIsEdited={setIsEdited}
+        />
       </table>
     </>
   );
