eb393d421162d16d7e575ac28e84a42a86eeb173
AbushaevaDiana <79981087+AbushaevaDiana@users.noreply.github.com>
2023-11-17 07:36:46 +0300

    44870677 Добавить бизнес-логику настроек расписания и справочников по преподавателям

--- Frontend/src/pages/Settings/TimetableSettingsPage/model/reducer.ts
+++ Frontend/src/pages/Settings/TimetableSettingsPage/model/reducer.ts
@@ -3,54 +3,60 @@ import { TimetableSettingsPageData, TimetableSettingsPageParadeData, TimetableSe
 
 const initPairData: TimetableSettingsPageTimeData = [
   {
-    "id": 0,
-    "start_time": "12.00",
-    "end_time": "13.30"
+    id: "0",
+    startTime: "12.00",
+    endTime: "13.30"
   },
 
   {
-    "id": 1,
-    "start_time": "10.30",
-    "end_time": "12.00"
+    id: "1",
+    startTime: "10.30",
+    endTime: "12.00"
   },
 ];
 
 const initLessonData: TimetableSettingsPageTimeData = [
   {
-    "id": 0,
-    "start_time": "8.00",
-    "end_time": "8.45"
+    id: "3",
+    startTime: "8.00",
+    endTime: "8.45"
   },
 
-  {"id": 1,
-    "start_time": "9.00",
-    "end_time": "9.45"
+  {
+    id: "4",
+    startTime: "9.00",
+    endTime: "9.45"
   },
 ];
 
 const initParadeData: TimetableSettingsPageParadeData = {
-  "week_day": "Понедельник",
-  "start_time": "8.00",
-  "end_time": "8.45"
+  weekDay: "Понедельник",
+  startTime: "8.00",
+  endTime: "8.45"
 };
 
 const initData: TimetableSettingsPageData = {
-  "pairs": initPairData,
-  "lessons": initLessonData,
-  "parade": initParadeData,
+  pairs: initPairData,
+  lessons: initLessonData,
+  parade: initParadeData,
 };
 
 const timetableSettingsPageReducer = (state = initData, action: Action) => {
   switch (action.type) {
-    case TIMETABLE_SETTINGS_PAGE_ACTIONS.TIMETABLE_SETTINGS_PAGE_DELETE_PAIR:
+    case TIMETABLE_SETTINGS_PAGE_ACTIONS.TIMETABLE_SETTINGS_PAGE_SAVE_PAIRS:
       return {
         ...state,
-        pairs: state.pairs.filter((item) => item.id !== action.payload),
+        pairs: action.payload,
       };
-    case TIMETABLE_SETTINGS_PAGE_ACTIONS.TIMETABLE_SETTINGS_PAGE_DELETE_LESSON:
+    case TIMETABLE_SETTINGS_PAGE_ACTIONS.TIMETABLE_SETTINGS_PAGE_SAVE_LESSONS:
       return {
         ...state,
-        lessons: state.lessons.filter((item) => item.id !== action.payload),
+        lessons: action.payload,
+      };
+    case TIMETABLE_SETTINGS_PAGE_ACTIONS.TIMETABLE_SETTINGS_PAGE_SAVE_PARADE:
+      return {
+        ...state,
+        parade: action.payload,
       };
     default:
       return state;
--- Frontend/src/pages/Timetable/TeacherGuidebookTimetablePage/TeacherGuidebookTimetablePage.tsx
+++ Frontend/src/pages/Timetable/TeacherGuidebookTimetablePage/TeacherGuidebookTimetablePage.tsx
@@ -1,95 +1,151 @@
 import React, {useState} from "react";
-import {useSelector} from "react-redux";
-import {TeacherGuidebookTimetablePageData} from "./model/types";
-import Input, {InputType} from "../../../components/Input/Input";
+import {useDispatch, useSelector} from "react-redux";
+import {Guidebook, TeacherGuidebookTimetableData, TeacherGuidebookTimetablePageData} from "./model/types";
+import Input, {InputSize, InputType} from "../../../components/Input/Input";
 import ActionButton, { ActionButtonType } from "../../../components/ActionButton/ActionButton";
 import {PenIcon} from "../../../icons";
 import { classNames } from "../../../utils/classNames";
-
-const Guidebook = () => {
-  const guidebook = useSelector((state: { teacherGuidebookTimetablePageStore: TeacherGuidebookTimetablePageData }) => state.teacherGuidebookTimetablePageStore);
-
-  return (
-    guidebook.map((teacher) => (
-      <tr className="row" key={teacher.id}>
-        <td className="cell">{teacher.subjectName}</td>
-        <td className="cell">{teacher.teacherName}</td>
-        <td className="cell">
-          {teacher.availableHours.map((time) => (<p key={time.id}>{time.weekDay}  {time.startTime} - {time.endTime}</p>))}
-        </td>
-        <td className={classNames("cell" + (teacher.distributedHoursToPlan < teacher.hoursToPlan ? " -error" : "")
-        + (teacher.distributedHoursToPlan > teacher.hoursToPlan ? " -warning" : ""))}>
-          {teacher.distributedHoursToPlan}
-        </td>
-        <td className="cell">{teacher.hoursToPlan}</td>
-        <td className="cell">{teacher.creditHours}</td>
-        <td className="cell">{teacher.workedOverPlan}</td>
-      </tr>
-    ))
-  );
-};
-
-const TeacherGuidebookTimetableButton = (isEdited: boolean) => {
-  if (!isEdited) {
-    return (
-      <ActionButton
-        className="toolbar__button"
-        label="Редактировать"
-        icon={<PenIcon />}
-        onClick={() => alert("Редактировать")}
-      />
-    );
-  }
-  return (
-    <div className="toolbar__buttons-box">
-      <ActionButton
-        className="toolbar__button"
-        type={ActionButtonType.Positive}
-        label="Сохранить"
-        onClick={() => alert("Сохранить")}
-      />
-      <ActionButton
-        className="toolbar__button"
-        type={ActionButtonType.Negative}
-        label="Отменить"
-        onClick={() => alert("Отменить")}
-      />
-    </div>
-  );
-};
+import { CTableBuilder } from "../../../core/Table/CTableBuilder";
+import { CTable } from "../../../core/Table/CTable";
+import { CTableManager } from "../../../core/Table/CTableManager";
+import { TableType } from "../../../core/Table/TableType";
+import { TeacherGuidebookTimetableActionBuilder } from "./model/actions";
+import Select, { ISelectOption, SelectSize } from "../../../components/Select/Select";
+import { SortingOrder } from "../../../core/Table/SortingOrder";
 
 const TeacherGuidebookTimetablePage = () => {
-  const [searchValue, setSearchValue] = useState<string>("");
-  const isEdited = false;
+  const { guidebook, availableHours } = useSelector((state: { teacherGuidebookTimetablePageStore: TeacherGuidebookTimetablePageData }) => state.teacherGuidebookTimetablePageStore);
+  const dispatch = useDispatch();
+
+  const [isGuidebookEditing, setIGuidebookEditing] = useState<{ value: boolean }>({ value: false });
+  const [guidebookTableData, setGuidebookTableData] = useState<Guidebook>(structuredClone(guidebook));
+  const [guidebookSearchQuery, setGuidebookSearchQuery] = useState<string>("");
+  const guidebookTableBuilder: CTableBuilder = new CTableBuilder(guidebookTableData, setGuidebookTableData);
+  guidebookTableBuilder.addEditFeature(isGuidebookEditing, setIGuidebookEditing);
+  guidebookTableBuilder.addSearchFeature();
+  const guidebookTable: CTable = guidebookTableBuilder.getTable();
+  const guidebookTableManager: CTableManager = new CTableManager(guidebookTable);
+
+  const handleSaveGuidebookTable = () => {
+    guidebookTableManager.invokeFunction("apply", TableType.Editable, [
+      (data: any[]) => dispatch(TeacherGuidebookTimetableActionBuilder.saveAvailableHours(data))
+    ]);
+  };
+  const handleResetGuidebookTable = () => {
+    guidebookTableManager.invokeFunction("cancel", TableType.Editable, [guidebook]);
+  };
+  const editGuidebookTable = (): void => {
+    guidebookTableManager.invokeFunction("edit", TableType.Editable, []);
+  };
+  const handleGuidebookSearch = (): void => {
+    guidebookTableManager.invokeFunction("search", TableType.Searchable, [
+      guidebookSearchQuery,
+      guidebook
+    ]);
+  };
+  const handleSort = (columnName: string): void => {
+    guidebookTableManager.invokeFunction("sort", TableType.Default, [columnName, SortingOrder.Ascending]);
+  };
+
+  const hoursOptions: ISelectOption[] = availableHours.map((hour) => ({ id: hour.id, content: hour.weekDay + " " + hour.startTime + "-" + hour.endTime }));
 
   return (
     <>
       <div className="toolbar">
         <div className="toolbar__buttons-wrapper">
-          {TeacherGuidebookTimetableButton(isEdited)}
+          {isGuidebookEditing.value ? (
+            <div className="toolbar__buttons-box">
+              <ActionButton
+                className="toolbar__button"
+                type={ActionButtonType.Positive}
+                label="Сохранить"
+                onClick={handleSaveGuidebookTable}
+              />
+              <ActionButton
+                className="toolbar__button"
+                type={ActionButtonType.Negative}
+                label="Отменить"
+                onClick={handleResetGuidebookTable}
+              />
+            </div>
+          ) : (
+            <ActionButton
+              className="toolbar__button"
+              label="Редактировать"
+              icon={<PenIcon />}
+              onClick={editGuidebookTable}
+            />
+          )}
+          
           <Input
             className="toolbar__search"
-            value={searchValue}
+            value={guidebookSearchQuery}
             type={InputType.Search}
-            placeholder="Поиск"
-            onValueChange={setSearchValue}
+            placeholder="Поиск"        
+            onValueChange={setGuidebookSearchQuery}
+            size={InputSize.Default}
+            onSearch={handleGuidebookSearch}
           />
         </div>
       </div>
-      <table className="table">
+      <table className="table -fill -list">
         <thead className="header">
           <tr className="row">
-            <th className="cell">Предмет</th>
-            <th className="cell">Преподаватель</th>
+            <th className="cell -filter" onClick={() => handleSort("subjectName")}>Предмет</th>
+            <th className="cell -filter" onClick={() => handleSort("teacherName")}>Преподаватель</th> 
             <th className="cell">Доступные<br />часы</th>
-            <th className="cell">Кол-во распределенных<br />часов в неделю по плану</th>
-            <th className="cell">Кол-во часов<br />в неделю по плану</th>
-            <th className="cell">Кол-во часов<br />долга</th>
-            <th className="cell">Кол-во часов<br />переработка</th>
+            <th className="cell -filter" onClick={() => handleSort("distributedHoursToPlan")}>Кол-во распределенных<br />часов в неделю по плану</th>
+            <th className="cell -filter" onClick={() => handleSort("hoursToPlan")}>Кол-во часов<br />в неделю по плану</th>
+            <th className="cell -filter" onClick={() => handleSort("creditHours")}>Кол-во часов<br />долга</th>
+            <th className="cell -filter" onClick={() => handleSort("workedOverPlan")}>Кол-во часов<br />переработка</th>
           </tr>
         </thead>
         <tbody>
-          {Guidebook()}
+          {guidebookTableData.filter((data: TeacherGuidebookTimetableData, index: number) =>
+            index !== guidebookTableData.length).map((teacher: TeacherGuidebookTimetableData) => {
+            return (
+              <tr className="row" key={teacher.id}>
+                <td className="cell">{teacher.subjectName}</td>
+                <td className="cell">{teacher.teacherName}</td>
+                <td className="cell">
+                  {isGuidebookEditing.value ? (
+                    <Select
+                      currentValue={hoursOptions.find(e => e.id === teacher.availableHours.id)}
+                      options={hoursOptions}
+                      onValueChange={(newValue: string) => {
+                        const selectedOption = hoursOptions.find(e => e.id === newValue);
+                        if (selectedOption) {
+                          setGuidebookTableData(
+                            guidebookTableData.map((data: TeacherGuidebookTimetableData) =>
+                              data.id === teacher.id ? {
+                                ...data,
+                                availableHours: {
+                                  id: selectedOption.id,
+                                  weekDay: selectedOption.content.substring(0, 2),
+                                  startTime: selectedOption.content.substring(3, 7),
+                                  endTime: selectedOption.content.substring(8),
+                                }
+                              } : data
+                            )
+                          );
+                        }
+                      }}
+                      size={SelectSize.Micro}
+                    />
+                  ) : (
+                    <p>{teacher.availableHours.weekDay} {teacher.availableHours.startTime}-{teacher.availableHours.endTime}</p>
+                  )}
+                </td>
+                <td className={classNames("cell" + (teacher.distributedHoursToPlan < teacher.hoursToPlan ? " -error" : "")
+                    + (teacher.distributedHoursToPlan > teacher.hoursToPlan ? " -warning" : ""))}>
+                  {teacher.distributedHoursToPlan}
+                </td>
+                <td className="cell">{teacher.hoursToPlan}</td>
+                <td className="cell">{teacher.creditHours}</td>
+                <td className="cell">{teacher.workedOverPlan}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </>
