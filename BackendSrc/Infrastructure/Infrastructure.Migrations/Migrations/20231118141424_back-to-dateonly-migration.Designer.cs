4fd26e40cf52c0391d400e7aef7340bd053b7b4d
Pavel Ermakov <pavel.ermakov.acc@outlook.com>
2023-11-18 17:21:01 +0300

    MEG-31 Добавил миграцию по переезду с DateTime на DateOnly

--- BackendSrc/Infrastructure/Infrastructure.Migrations/Migrations/20231118141424_back-to-dateonly-migration.Designer.cs
+++ BackendSrc/Infrastructure/Infrastructure.Migrations/Migrations/20231118141424_back-to-dateonly-migration.Designer.cs
@@ -4,6 +4,7 @@ using DatabaseProvider;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20231118141424_back-to-dateonly-migration")]
+    partial class backtodateonlymigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
