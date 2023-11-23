89723d012786ab9ca325d8926444bee3f5fe0ecd
Yaroslav <Yaroslav.Kuzmin.01@yandex.ru>
2023-11-24 00:11:34 +0300

    44847565 Вынес в компоненты

---
+++ Frontend/src/pages/CoursesTimetable/TeacherGuidebookCoursesTimetablePage/view/TeacherGuidebookTimetableTable.tsx
@@ -0,0 +1,87 @@
+import {TeacherGuidebookCoursesTimetablePageData} from "../model/types";
+import {classNames} from "../../../../utils/classNames";
+import React from "react";
+
+interface TeacherGuidebookTimetableTableProps {
+  data: TeacherGuidebookCoursesTimetablePageData,
+  handleSort: (columnName: string) => void,
+}
+
+const TeacherGuidebookTimetableTable = ({
+  data,
+  handleSort,
+}: TeacherGuidebookTimetableTableProps) => {
+  const rows = data.map((item) => (
+    <tr className="row" key={item.id}>
+      <td className="cell">
+        {item.course}
+      </td>
+      <td className="cell">{item.type}</td>
+      <td className="cell">{item.teacherName}</td>
+      <td className="cell">
+        {item.availableHours.map((time) => (
+          <p key={time.id}>{time.weekDay} {time.startTime} - {time.endTime}</p>))}
+      </td>
+      <td className={classNames("cell" + (item.distributedHoursToPlan < item.hoursToPlan ? " -error" : "")
+        + (item.distributedHoursToPlan > item.hoursToPlan ? " -warning" : ""))}>
+        {item.distributedHoursToPlan}
+      </td>
+      <td className="cell">{item.hoursToPlan}</td>
+      <td className="cell">{item.creditHours}</td>
+    </tr>
+  ));
+
+  return <>
+    <thead className="header">
+      <tr className="row">
+        <th
+          className="cell -filter"
+          onClick={() => handleSort("course")}
+        >
+        Курс
+        </th>
+        <th
+          className="cell -filter"
+          onClick={() => handleSort("type")}
+        >
+        Тип
+        </th>
+        <th
+          className="cell -filter"
+          onClick={() => handleSort("teacherName")}
+        >
+        Преподаватель
+        </th>
+        <th
+          className="cell -filter"
+          onClick={() => handleSort("availableHours")}
+        >
+        Доступные часы
+        </th>
+        <th
+          className="cell -filter"
+          onClick={() => handleSort("distributedHoursToPlan")}
+        >
+        Кол-во распределенных часов в неделю по плану
+        </th>
+        <th
+          className="cell -filter"
+          onClick={() => handleSort("hoursToPlan")}
+        >
+        Кол-во часов в неделю по плану
+        </th>
+        <th
+          className="cell -filter"
+          onClick={() => handleSort("creditHours")}
+        >
+        Кол-во часов долга
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      {rows}
+    </tbody>
+  </>;
+};
+
+export default TeacherGuidebookTimetableTable;
