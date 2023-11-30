cf7d6818eb9a198861f75b2b1e2efe509d5d84b8
Maxim <masya.prozorov@mail.ru>
2023-11-30 19:59:56 +0300

    MEG-40 Внес правки после ревью, изменил структуру

--- BackendSrc/Api/Models/Settings/SalarySettings/SalarySettingsDto.cs
+++ BackendSrc/Api/Models/Settings/SalarySettings/SalarySettingsDto.cs
@@ -1,13 +1,7 @@
 using System.Text.Json.Serialization;
 
-namespace Api.Models.EductionPlanSettings.SalarySettings
+namespace Api.Models.Settings.SalarySettings
 {
-    public class SalariesSettingsDto
-    {
-        [JsonPropertyName( "salaries" )]
-        public List<SalarySettingsDto> Salaries { get; set; }
-    }
-
     public class SalarySettingsDto
     {
         [JsonPropertyName( "id" )]
