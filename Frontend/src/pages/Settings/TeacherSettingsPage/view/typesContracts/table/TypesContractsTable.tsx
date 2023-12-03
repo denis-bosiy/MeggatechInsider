0a34f141f98a0a2a4bf9fef57504aeac5ac2383e
denis.bosyi <denis.bosiy@gmail.com>
2023-12-04 00:16:58 +0300

    Добавлена мягкая интеграция на страницу настроек преподавателей

--- Frontend/src/pages/Settings/TeacherSettingsPage/view/typesContracts/table/TypesContractsTable.tsx
+++ Frontend/src/pages/Settings/TeacherSettingsPage/view/typesContracts/table/TypesContractsTable.tsx
@@ -1,54 +1,70 @@
 import React from "react";
 import IconButton from "../../../../../../components/IconButton/IconButton";
-import "./TypesContractsTable.scss";
-import {useDispatch, useSelector} from "react-redux";
-import {GarbageIcon} from "../../../../../../icons";
-import {TypesContractsItem} from "../model/types";
-import {TypesContractsActionBuilder} from "../model/actions";
+import { CheckIcon, GarbageIcon } from "../../../../../../icons";
+import { TypesContractsItem } from "../model/types";
+import Input, { InputSize } from "../../../../../../components/Input/Input";
 
-interface TypesContractsCellProps {
-  label: string,
-  icon: React.ReactNode,
+interface ITypesContractsTableProps {
+  items: TypesContractsItem[];
+  handleDeleteRow(id: string): void;
+  isAdding: boolean;
+  handleApplying(): void;
+  setItems(items: TypesContractsItem[]): void;
+  handleSort(columnName: string): void;
 }
-const TypesContractsCell = ({
-  label,
-  icon
-}: TypesContractsCellProps) => {
-  return <div className="types-contracts-cell-types">
-    {label}
-    {icon}
-  </div>;
-};
 
-const TypesContractsTable = () => {
-  const items = useSelector(
-    (state: { typesContractsStore: TypesContractsItem[] }) => state.typesContractsStore
-  );
-  const dispatch = useDispatch();
-
-  const rows = items.map((item, index) => <tr className="row" key={`${item.id}-${index}`}>
-    <td className="cell">
-      <TypesContractsCell
-        label={item.name}
-        icon={<IconButton
-          icon={<GarbageIcon />}
-          small={true}
-          onClick={() => dispatch(TypesContractsActionBuilder.deleteItem(item.id))}
-        />}
-      />
-    </td>
-  </tr>);
-
-  return <table className="table -fill -list">
-    <thead className="header">
-      <tr className="row">
-        <th className="cell">Типы договоров</th>
+const TypesContractsTable = (props: ITypesContractsTableProps) => {
+  const rows = props.items
+    .filter((item: TypesContractsItem, index: number) => !props.isAdding || index !== props.items.length - 1)
+    .map((item, index) => (
+      <tr className="row" key={`${item.id}-${index}`}>
+        <td className="cell">{item.name}</td>
+        <td className="cell">
+          <IconButton icon={<GarbageIcon />} small={true} onClick={() => props.handleDeleteRow(item.id)} />
+        </td>
       </tr>
-    </thead>
-    <tbody>
-      {rows}
-    </tbody>
-  </table>;
+    ));
+
+  return (
+    <table className="table -fill -list">
+      <thead className="header">
+        <tr className="row">
+          <th className="cell -filter" onClick={() => props.handleSort("name")}>
+            Типы договоров
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}{" "}
+        {props.isAdding && (
+          <tr className="row">
+            <td className="cell">
+              <Input
+                size={InputSize.Micro}
+                value={props.items[props.items.length - 1].name}
+                placeholder=""
+                onValueChange={(newNameValue: string) => {
+                  props.setItems(
+                    props.items.map((value: TypesContractsItem, index: number) =>
+                      index === props.items.length - 1
+                        ? {
+                          id: value.id,
+                          name: newNameValue
+                        }
+                        : value
+                    )
+                  );
+                }}
+              />
+            </td>
+            <td className="cell">
+              <IconButton icon={<CheckIcon />} small={true} onClick={() => props.handleApplying()} />
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
 };
 
 export default TypesContractsTable;
