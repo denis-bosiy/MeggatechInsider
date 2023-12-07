25e3aac0ed770b071cfc5a28cd1a0745ae5212db
Denis Bosyi <48645130+denis-bosiy@users.noreply.github.com>
2023-12-08 00:36:09 +0300

    46427412 Добавить мягкую интеграцию на страницу настроек преподавателей

--- Frontend/src/pages/Settings/TeacherSettingsPage/view/teacherEducation/table/TeacherEducationTable.tsx
+++ Frontend/src/pages/Settings/TeacherSettingsPage/view/teacherEducation/table/TeacherEducationTable.tsx
@@ -1,67 +1,98 @@
 import React from "react";
 import IconButton from "../../../../../../components/IconButton/IconButton";
-import {GarbageIcon} from "../../../../../../icons";
-import "./TeacherEducationTable.scss";
-import {useDispatch, useSelector} from "react-redux";
-import {TeacherEducationItem} from "../model/types";
-import {TeacherEducationActionBuilder} from "../model/actions";
+import { CheckIcon, GarbageIcon } from "../../../../../../icons";
+import { TeacherEducationItem } from "../model/types";
+import Input, { InputSize } from "../../../../../../components/Input/Input";
 
-interface TeacherEducationEducationCellProps {
-  label: string,
+interface ITeacherEducationTableProps {
+  items: TeacherEducationItem[];
+  handleDeleteRow(id: string): void;
+  isAdding: boolean;
+  handleApplying(): void;
+  setItems(items: TeacherEducationItem[]): void;
+  handleSort(columnName: string): void;
 }
 
-const TeacherEducationEducationCell = ({
-  label,
-}: TeacherEducationEducationCellProps) => {
-  return <>{label}</>;
-};
-
-interface TeacherEducationCoefficientCellProps {
-  coefficient: number,
-  icon: React.ReactNode,
-}
-
-const TeacherEducationCoefficientCell = ({
-  coefficient,
-  icon,
-}: TeacherEducationCoefficientCellProps) => {
-  return <div className="teacher-education-coefficient-cell">
-    {coefficient}
-    {icon}
-  </div>;
-};
-
-const TeacherEducationTable = () => {
-  const items = useSelector(
-    (state: { teacherEducationStore: TeacherEducationItem[] }) => state.teacherEducationStore
-  );
-  const dispatch = useDispatch();
-
-  const rows = items.map((item, index) => <tr className="row" key={`${item.id}-${index}`}>
-    <td className="cell"><TeacherEducationEducationCell label={item.education}/></td>
-    <td className="cell">
-      <TeacherEducationCoefficientCell
-        coefficient={item.coefficient}
-        icon={<IconButton
-          icon={<GarbageIcon />}
-          small={true}
-          onClick={() => dispatch(TeacherEducationActionBuilder.deleteItem(item.id))}
-        />}
-      />
-    </td>
-  </tr>);
-
-  return <table className="table -fill -list">
-    <thead className="header">
-      <tr className="row">
-        <th className="cell -filter">Образование преподавателей</th>
-        <th className="cell">Коэффициент</th>
+const TeacherEducationTable = (props: ITeacherEducationTableProps) => {
+  const rows = props.items
+    .filter((item: TeacherEducationItem, index: number) => !props.isAdding || index !== props.items.length - 1)
+    .map((item, index) => (
+      <tr className="row" key={`${item.id}-${index}`}>
+        <td className="cell">{item.education}</td>
+        <td className="cell">{item.coefficient}</td>
+        <td className="cell">
+          <IconButton icon={<GarbageIcon />} small={true} onClick={() => props.handleDeleteRow(item.id)} />
+        </td>
       </tr>
-    </thead>
-    <tbody>
-      {rows}
-    </tbody>
-  </table>;
+    ));
+
+  return (
+    <table className="table -fill -list">
+      <thead className="header">
+        <tr className="row">
+          <th className="cell -filter" onClick={() => props.handleSort("education")}>
+            Образование преподавателей
+          </th>
+          <th className="cell">Коэффициент</th>
+          <th className="cell"></th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+        {props.isAdding && (
+          <tr className="row">
+            <td className="cell">
+              <Input
+                size={InputSize.Micro}
+                value={props.items[props.items.length - 1].education}
+                placeholder=""
+                onValueChange={(newEducationValue: string) => {
+                  props.setItems(
+                    props.items.map((value: TeacherEducationItem, index: number) =>
+                      index === props.items.length - 1
+                        ? {
+                          id: value.id,
+                          education: newEducationValue,
+                          coefficient: value.coefficient
+                        }
+                        : value
+                    )
+                  );
+                }}
+              />
+            </td>
+            <td className="cell">
+              <Input
+                size={InputSize.Micro}
+                value={
+                  props.items[props.items.length - 1].coefficient
+                    ? props.items[props.items.length - 1].coefficient.toString()
+                    : ""
+                }
+                placeholder=""
+                onValueChange={(newCoefficientValue: string) => {
+                  props.setItems(
+                    props.items.map((value: TeacherEducationItem, index: number) =>
+                      index === props.items.length - 1
+                        ? {
+                          id: value.id,
+                          education: value.education,
+                          coefficient: parseFloat(newCoefficientValue)
+                        }
+                        : value
+                    )
+                  );
+                }}
+              />
+            </td>
+            <td className="cell">
+              <IconButton icon={<CheckIcon />} small={true} onClick={() => props.handleApplying()} />
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
 };
 
 export default TeacherEducationTable;
