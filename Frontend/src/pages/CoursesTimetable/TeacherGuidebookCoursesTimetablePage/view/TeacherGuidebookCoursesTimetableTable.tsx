24e7ba50148a876c8e3ed68ed0115857a6c6b478
Yaroslav <Yaroslav.Kuzmin.01@yandex.ru>
2023-12-08 04:38:41 +0300

    44902016 Поправил форматирование

--- Frontend/src/pages/CoursesTimetable/TeacherGuidebookCoursesTimetablePage/view/TeacherGuidebookCoursesTimetableTable.tsx
+++ Frontend/src/pages/CoursesTimetable/TeacherGuidebookCoursesTimetablePage/view/TeacherGuidebookCoursesTimetableTable.tsx
@@ -59,8 +59,11 @@ const TeacherGuidebookCoursesTimetableTable = ({
               ))}
           </>}
       </td>
-      <td className={classNames("cell" + (item.distributedHoursToPlan < item.hoursToPlan ? " -error" : "")
-        + (item.distributedHoursToPlan > item.hoursToPlan ? " -warning" : ""))}>
+      <td className={classNames(
+        "cell" +
+        (item.distributedHoursToPlan < item.hoursToPlan ? " -error" : "") +
+        (item.distributedHoursToPlan > item.hoursToPlan ? " -warning" : ""))}
+      >
         {item.distributedHoursToPlan}
       </td>
       <td className="cell">{item.hoursToPlan}</td>
