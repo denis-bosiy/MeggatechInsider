0ac1445c10b35809af343c7e85d0f0fa8c455e44
TRAVELLINE\denis.bosyi <denis.bosyi@travelline.ru>
2023-12-13 20:17:08 +0300

    Поменял текст на странице 404

--- Frontend/src/pages/NotFoundPage/NotFoundPage.tsx
+++ Frontend/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -6,7 +6,7 @@ import { AppRouter } from "../../router";
 const NotFoundPage = () => {
   return (
     <div className="not-found-page">
-      <h1 className="h1 not-found-page__heading">Такой страницы не существует</h1>
+      <h1 className="h1 not-found-page__heading">Такой страницы нет</h1>
 
       <Link path={AppRouter.Main} label="Перейти в меню" />
     </div>
