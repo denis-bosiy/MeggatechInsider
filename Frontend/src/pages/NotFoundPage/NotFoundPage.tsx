5fd82a0030d265ad4c8d29fe2562692b58d7578d
denis.bosyi <denis.bosiy@gmail.com>
2023-12-21 13:42:41 +0300

    Изменена страница 404

--- Frontend/src/pages/NotFoundPage/NotFoundPage.tsx
+++ Frontend/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./NotFoundPage.scss";
-import { Link } from "../../components/Link/Link";
+import { Link, LinkType } from "../../components/Link/Link";
 import { AppRouter } from "../../router";
 
 const NotFoundPage = () => {
   return (
     <div className="not-found-page">
-      <h1 className="h1 not-found-page__heading">Такой страницы нет</h1>
+      <div className="not-found-page__info">
+        <h1 className="h1 not-found-page__heading">Такой страницы нет</h1>
 
-      <Link path={AppRouter.Main} label="Перейти в меню" />
+        <Link path={AppRouter.Main} label="Перейти в меню" />
+      </div>
     </div>
   );
 };
