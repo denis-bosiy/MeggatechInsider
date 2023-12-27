e93ba71b45b9ae7ba33291b4a29bf7e955459357
Диана Абушаева <abu13.di@gmail.com>
2023-12-27 04:39:45 +0300

    delete too much sets

--- Frontend/src/pages/Syllabus/AssigningSyllabusPage/AssigningSyllabusPage.tsx
+++ Frontend/src/pages/Syllabus/AssigningSyllabusPage/AssigningSyllabusPage.tsx
@@ -1,13 +1,11 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   AssigningSyllabusPageData,
   AssigningSyllabusData,
   AssigningsSyllabusData,
-  DiscrepanciesSyllabusData
 } from "./model/types";
 import { ActionBuilder } from "./model/actions";
-import { classNames } from "../../../utils/classNames";
 import Input, { InputSize, InputType } from "../../../components/Input/Input";
 import ActionButton, { ActionButtonType } from "../../../components/ActionButton/ActionButton";
 import { CheckMarkIcon, GarbageIcon, PenIcon, PlusIcon } from "../../../icons";
@@ -20,67 +18,6 @@ import { CTableManager } from "../../../core/Table/CTableManager";
 import { TableType } from "../../../core/Table/TableType";
 import { guidGenerator } from "../../../utils/guidGenerator";
 import { SortingOrder } from "../../../core/Table/SortingOrder";
-import { HeaderData } from "../../../layouts/Header/model/types";
-import { HttpService } from "../../../api/http.service";
-import { Endpoint } from "../../../api/endpoints";
-import { DiscrepancyResponse } from "../../../api/Responses/DiscrepancyResponse";
-import { ResponseBuilder } from "../../../api/Responses/ResponseBuilder";
-
-const DiscrepanciesSyllabus = () => {
-  const httpService = new HttpService();
-  const { currentYear } = useSelector((state: { headerStore: HeaderData }) => state.headerStore);
-  const dispatch = useDispatch();
-  const data = useSelector(
-    (state: { assigningSyllabusPageStore: AssigningSyllabusPageData }) => state.assigningSyllabusPageStore
-  );
-  const discrepancies = data.discrepancies;
-
-  useEffect(() => {
-    const params: Map<string, string> = new Map<string, string>();
-    if (currentYear) {
-      params.set("year", currentYear.year.toString());
-    }
-
-    httpService
-      .getByArbitraryUrl(Endpoint.SyllabusDiscrepancies, params)
-      .then((data: any) => {
-        const discrepanciesResponse: DiscrepancyResponse[] = ResponseBuilder.BuildDiscrepanciesResponse(data);
-        const discrepancies: DiscrepanciesSyllabusData = discrepanciesResponse.map(
-          (discrepancy: DiscrepancyResponse) => {
-            return {
-              id: guidGenerator(),
-              name: discrepancy.name,
-              groupCount: discrepancy.groupCount,
-              groupCountByPlan: discrepancy.groupCountByPlan
-            };
-          }
-        );
-        dispatch(ActionBuilder.saveDiscrepancies(discrepancies));
-      })
-      .catch((e: any) => {
-        dispatch(ActionBuilder.saveDiscrepancies([]));
-      });
-  }, [currentYear?.id]);
-
-  return (
-    <>
-      {discrepancies.map((discrepancy) => (
-        <tr className="row" key={discrepancy.id}>
-          <td className="cell">{discrepancy.name}</td>
-          <td
-            className={classNames(
-              "cell" +
-                (discrepancy.groupCount < discrepancy.groupCountByPlan ? " -error" : "") +
-                (discrepancy.groupCount > discrepancy.groupCountByPlan ? " -warning" : "")
-            )}
-          >
-            {discrepancy.groupCount}
-          </td>
-        </tr>
-      ))}
-    </>
-  );
-};
 
 const AssigningSyllabusPage = () => {
   const subjectOptions: ISelectOption[] = [
@@ -221,39 +158,6 @@ const AssigningSyllabusPage = () => {
                   <br />
                   групп
                 </th>
-                <th className="cell -filter" onClick={() => handleSort("hoursByPlanOnClassOfTheStudents")}>
-                  Часов по плану на
-                  <br />
-                  класс
-                </th>
-                <th className="cell -filter" onClick={() => handleSort("hoursOnWeekForTheClassOfTheStudents")}>
-                  Часов в неделю на класс
-                </th>
-                <th className="cell -filter" onClick={() => handleSort("hoursOnWeekOnYearOnTheTeacher")}>
-                  Часов в неделю в<br />
-                  год на
-                  <br />
-                  препод-я
-                </th>
-                <th className="cell -filter" onClick={() => handleSort("hoursOnWeekOnPeriodOnTheTeacher")}>
-                  Часов в неделю в<br />
-                  период на
-                  <br />
-                  препод-я
-                </th>
-                <th className="cell -filter" onClick={() => handleSort("hoursIn1Subgroup")}>
-                  Часов в 1 пг.
-                </th>
-                <th className="cell -filter" onClick={() => handleSort("hoursIn2Subgroup")}>
-                  Часов во 2 пг.
-                </th>
-                <th className="cell -filter" onClick={() => handleSort("totalInYear")}>
-                  Всего
-                  <br />в год
-                </th>
-                <th className="cell -filter" onClick={() => handleSort("bidShare")}>
-                  Доля ставки
-                </th>
               </tr>
             </thead>
             <tbody>
@@ -326,30 +230,6 @@ const AssigningSyllabusPage = () => {
                         )}
                       </td>
                       <td className="cell">
-                        {value.hoursByPlanOnClassOfTheStudents}
-                      </td>
-                      <td className="cell">
-                        {value.hoursOnWeekForTheClassOfTheStudents}
-                      </td>
-                      <td className="cell">
-                        {value.hoursOnWeekOnYearOnTheTeacher}
-                      </td>
-                      <td className="cell">
-                        {value.hoursOnWeekOnPeriodOnTheTeacher}
-                      </td>
-                      <td className="cell">
-                        {value.hoursIn1Subgroup}
-                      </td>
-                      <td className="cell">
-                        {value.hoursIn2Subgroup}
-                      </td>
-                      <td className="cell">
-                        {value.totalInYear}
-                      </td>
-                      <td className="cell">
-                        {value.bidShare}
-                      </td>
-                      <td className="cell">
                         <IconButton icon={<GarbageIcon />} onClick={() => handleDeleteAssigning(value.id.toString())} />
                       </td>
                     </tr>
@@ -410,14 +290,6 @@ const AssigningSyllabusPage = () => {
                       size={InputSize.Micro}
                     />
                   </td>
-                  <td className="cell"></td>
-                  <td className="cell"></td>
-                  <td className="cell"></td>
-                  <td className="cell"></td>
-                  <td className="cell"></td>
-                  <td className="cell"></td>
-                  <td className="cell"></td>
-                  <td className="cell"></td>
                   <td className="cell">
                     <IconButton
                       icon={<CheckMarkIcon />}
@@ -427,20 +299,6 @@ const AssigningSyllabusPage = () => {
                   </td>
                 </tr>
               )}
-            </tbody>
-          </table>
-        </div>
-        <div className="table-wrapper">
-          <h2 className="h2 table-wrapper__title">Расхождения</h2>
-          <table className="table -fill -list">
-            <thead className="header">
-              <tr className="row">
-                <th className="cell">Предмет</th>
-                <th className="cell">Число групп</th>
-              </tr>
-            </thead>
-            <tbody>
-              <DiscrepanciesSyllabus />
             </tbody>
           </table>
         </div>
